5ffe208325352d378d616c1510f908681560ee3f
Wouter <wouter@xesxen.nl>
2020-04-17 15:19:41 +0200

    Add MQTT and NTP controls, move Mumble config to WiFiConfig

--- src/main.ino
+++ src/main.ino
@@ -3,9 +3,32 @@
 #include <WiFi.h>
 #include <WiFiConfig.h>
 #include <WiFiUdp.h>
+#include <M5Atom.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <NTPClient.h>
 
-const char* address = "revspace.nl";
-const int port = 64738;
+String mumble_host;
+unsigned int mumble_port;
+
+bool display_always_on;
+
+bool mqtt_enabled;
+bool mqtt_control_display;
+String mqtt_host;
+unsigned int mqtt_port;
+String mqtt_on;
+String mqtt_off;
+String mqtt_topic;
+String mqtt_topic_key;
+String mqtt_topic_control;
+String mqtt_topic_get;
+String mqtt_topic_get_data;
+bool lampstate = false;
+
+bool display_hour_enabled;
+short display_hour_on;
+short display_hour_off;
 
 const int buttonpin = 39;
 const int ledpin = 27;
@@ -14,10 +37,16 @@ CRGB leds[numleds];
 uint8_t hue = 0;
 byte reqBuffer[12];
 char respBuffer[24];
-int connected = 0;
+uint32_t connected = 0;
 int lastTime = 0;
 
 WiFiUDP udp;
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+
+Button btn = Button(39, true, 10);
 
 struct __attribute__ ((packed)) mumbleRequest {
 	unsigned int command;
@@ -124,14 +153,11 @@ const bool sprites[] = {
 	X, _, _, _
 };
 
-void number(int num) {
-	int overflow = 0;
-	while (num > 15) {
-		num = num-16;
-		overflow += 1;
-	}
-
-	if (overflow > 5) {overflow = 5;}
+void number(uint32_t num) {
+    uint32_t overflow;
+	overflow = num >> 4;
+    num &= 0x0F;
+    if (overflow > 5) {overflow = 5;}
 
 	for (int y=0; y<5; y++) {
 		leds[20-y*5] = CRGB::Black;
@@ -143,6 +169,64 @@ void number(int num) {
 			leds[y*5+1+x] = sprites[num*5*4 + y*4 + x] ? CHSV((hue+y*5+x)%255, 255, 255) : CHSV(0, 0, 0);
 		}
 	}
+}
+
+void togglestate() {
+    if (mqtt_topic_control.length()) {
+        if (lampstate) {
+            mqttClient.publish(mqtt_topic_control.c_str(), mqtt_off.c_str());
+        } else {
+            mqttClient.publish(mqtt_topic_control.c_str(), mqtt_on.c_str());
+        }
+        lampstate = !lampstate;
+    } else if (!mqtt_enabled) {
+        lampstate = !lampstate;
+    }
+}
+
+void callback(char* topic, byte* p_payload, unsigned int p_length) {
+    String payload;
+    for (uint8_t i = 0; i < p_length; i++) {
+        payload.concat((char)p_payload[i]);
+    }
+    DynamicJsonDocument doc(200);
+    deserializeJson(doc, payload);
+
+    Serial.print("Got message: ");
+    Serial.print(payload);
+    Serial.print(" on topic ");
+    Serial.println(topic);
+
+    if(mqtt_on.equals(doc[mqtt_topic_key].as<const char*>())) {
+        lampstate = true;
+    } else if(mqtt_off.equals(doc[mqtt_topic_key].as<const char*>())) {
+        lampstate = false;
+    }
+}
+
+boolean reconnect() {
+    static long last = -5000;
+    long current = millis();
+    if (current - last < 5000) {
+        return false;
+    }
+    last = current;
+
+    Serial.println("MQTT reconnect");
+
+    String mqttClientId = "ESP8266Client-";
+    mqttClientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (mqttClient.connect(mqttClientId.c_str())) {
+        // Once connected, publish an announcement...
+        // ... and resubscribe
+        mqttClient.subscribe(mqtt_topic.c_str());
+        if (mqtt_topic_get.length()) {
+            mqttClient.publish(mqtt_topic_get.c_str(), mqtt_topic_get_data.c_str());
+        }
+        Serial.println("Connected!");
+    }
+    return mqttClient.connected();
 }
 
 void setup() {
@@ -165,19 +249,44 @@ void setup() {
 		color.saturation--;
 		FastLED.showColor(color);
 	};
-	WiFiConfig.connect();
 
-	udp.begin(port);
+    mqtt_enabled = WiFiConfig.checkbox("mqtt_enabled", true, "MQTT enabled. When disabled, clicking the screen turns it off/on");
+    mqtt_host = WiFiConfig.string("mqtt_host", 63, "", "MQTT server hostname / IP Address");
+    mqtt_port = WiFiConfig.integer("mqtt_port", 0, 65535, 1883, "MQTT server port");
+    mqtt_control_display = WiFiConfig.checkbox("mqtt_control_display", true, "MQTT state ON also sets the display on");
+    mqtt_topic = WiFiConfig.string("mqtt_topic", 63, "", "Device topic, eg zigbee2mqtt/devicename");
+    mqtt_topic_key = WiFiConfig.string("mqtt_topic_key", 63, "status", "JSON status key");
+    mqtt_on = WiFiConfig.string("mqtt_on", 63, "ON", "JSON status 'on' value");
+    mqtt_off = WiFiConfig.string("mqtt_on", 63, "OFF", "JSON status 'off' value");
+    mqtt_topic_get = WiFiConfig.string("mqtt_topic_get", 63, "", "Topic to trigger status request. Leave empty if the status is retained");
+    mqtt_topic_get_data = WiFiConfig.string("mqtt_topic_get_data", 63, "{\"state\":\"\"}", "Payload to send with get request");
+    mqtt_topic_control = WiFiConfig.string("mqtt_topic_set", 63, "", "Topic to write new status to device. Leave empty if unneeded. eg. zigbee2mqtt/devicename/set/state");
 
-	// number demoreel
-	// for (int i=0; i<5*16-1; i++) {
-	// 	for (int j=0; j<30; j++) {
-	// 		number(i);
-	// 		FastLED.show();
-	// 		hue += 1;
-	// 		delay(25);
-	// 	}
-	// }
+    if (mqtt_enabled) {
+        mqttClient.setServer(mqtt_host.c_str(), mqtt_port);
+        mqttClient.setCallback(callback);
+    } else {
+        lampstate = true;
+    }
+
+    mumble_host = WiFiConfig.string("mumble_host", 63, "revspace.nl", "Mumble hostname");
+    mumble_port = WiFiConfig.integer("mumble_port", 0, 65535, 64738, "Mumble port");
+
+    display_hour_enabled = WiFiConfig.checkbox("display_hour_enabled", true, "Have the display always on between certain hours");
+    display_hour_on = WiFiConfig.integer("display_hour_on", 7, "Enable the display from <i>value</i>:00");
+    display_hour_off = WiFiConfig.integer("display_hour_off", 20, "Enable the display till <i>value</i>:59");
+
+    btn.read();
+    if (btn.wasPressed()) {
+        WiFiConfig.portal();
+    }
+    WiFiConfig.connect();
+
+    udp.begin(mumble_port);
+
+    if (display_hour_enabled) {
+        timeClient.begin();
+    }
 }
 
 
@@ -189,7 +298,7 @@ void loop() {
 			.ident = millis()
 		};
 
-		udp.beginPacket(address, port);
+        udp.beginPacket(mumble_host.c_str(), mumble_port);
 		udp.write((const uint8_t*) &request, sizeof(request));
 		udp.endPacket();
 
@@ -205,8 +314,37 @@ void loop() {
 		Serial.println(" people are connected");
 	}
 
-	number(connected);
-	hue += 1;
-	FastLED.show();
-	delay(25);
+	if ((mqtt_control_display && lampstate) ||
+        (!mqtt_enabled && lampstate) || // Button controls display
+	    (display_hour_enabled && display_hour_on <= timeClient.getHours() && timeClient.getHours() <= display_hour_off) ||
+	    (!mqtt_control_display && !display_hour_enabled && mqtt_enabled)
+    ) {
+        number(connected);
+    } else {
+	    FastLED.clear();
+	}
+	static long lasthue = 0;
+	long now = millis();
+	if (now - lasthue > 15) {
+        hue += 1;
+        lasthue = now;
+    }
+
+    FastLED.show();
+
+	if (mqtt_enabled) {
+        if (!mqttClient.connected()) {
+            reconnect();
+        }
+        mqttClient.loop();
+    }
+
+    if (display_hour_enabled) {
+        timeClient.update();
+    }
+
+    btn.read();
+    if (btn.wasPressed()) {
+        togglestate();
+    }
 }
